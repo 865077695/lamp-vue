2b2ea0a284f2ceca476b8f9248d299432b903321
zhiq <865077695@qq.com>
2018-02-07 14:22:31 +0800

    添加年月日期数据初始化函数。用于返回当前所需年月范围

--- src/common/_func.js
+++ src/common/_func.js
@@ -3,6 +3,7 @@
  * @param {*当前类型码} typ
  * @param {*当前key对应的类型配置} options
  */
+import config from '@/config'
 export const getStatusText = (typ, options) => {    // 传入当前类型和类型配置数组，返回类型码对应的文字
   let label
   options.map(item => {
@@ -27,4 +28,58 @@ export const formmatDate = (date) => {    // 格式化日期格式
 export const formmatTime = (date) => {    // 格式化时间格式
   let time = new Date(date)
   return `${add0(time.getHours())}:${add0(time.getMinutes())}:${add0(time.getSeconds())}`
+}
+
+export const powerDate = () => {      // 返回能耗统计页面所用的年月待选数据
+  let startYear = config.startYear
+  let y = []
+  let time = new Date() // 当前年份
+  let year = time.getFullYear()
+  for (let i = 0; i < year - startYear + 1; i++) {
+    y.push(String(+startYear + i))
+  }
+  y = y.map(item => {
+    return {
+      value: item,
+      label: item,
+      children: [{
+        value: '01',
+        label: '01'
+      }, {
+        value: '02',
+        label: '02'
+      }, {
+        value: '03',
+        label: '03'
+      }, {
+        value: '04',
+        label: '04'
+      }, {
+        value: '05',
+        label: '05'
+      }, {
+        value: '06',
+        label: '06'
+      }, {
+        value: '07',
+        label: '07'
+      }, {
+        value: '08',
+        label: '08'
+      }, {
+        value: '09',
+        label: '09'
+      }, {
+        value: '10',
+        label: '10'
+      }, {
+        value: '11',
+        label: '11'
+      }, {
+        value: '12',
+        label: '12'
+      }]
+    }
+  })
+  return y
 }
--- src/config/index.js
+++ src/config/index.js
@@ -2,5 +2,6 @@ export default {
   // baseURL: 'https://easy-mock.com/mock/5a5dca6524f2fc35a9bf4992/lamp-sys'
   // baseURL: 'http://192.168.2.120:8888/casintelamp-web'
   // baseURL: 'http://192.168.1.107:8080'
-  baseURL: 'http://113.105.131.250:8320/casintelamp-web'
+  baseURL: 'http://113.105.131.250:8320/casintelamp-web',
+  startYear: '2017'     // 能耗统计开始年份，结束年份为当前年
 }
